3d3fc0141270ef00f08051a376a7e5e3a77b04d9
Parish Neighbors <pn8bors@gmail.com>
2022-02-08 11:31:29 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+import input from 'input';
+
+function square(size) {
+    for (let i = 1; i <= size; i += 1) {
+        console.log('X'.repeat(size));
+    }
+}
+
+async function main() {
+    const size = await input.text('Enter a size');
+    square(size);
+}
+
+main();
